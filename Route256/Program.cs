0da842ecb83baaea690830ad1948670e5ce53803
KeST3107 <kest3107@gmail.com>
2021-10-06 15:00:30 +0300

    Console tests

--- Route256/Program.cs
+++ Route256/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static Route256.TaskList;
 
 namespace Route256
 {
@@ -6,7 +7,12 @@ namespace Route256
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine(ATask("hEllo world!"));
+            Console.WriteLine(BTask("Привет мир!"));
+            Console.WriteLine(CTask("10.0.0.0 10.0.0.50"));
+            Console.WriteLine(DTask("1000 2000 1000 2000 5000"));
+            Console.WriteLine(ETask("MMLXII MMLX"));
+            Console.WriteLine(FTask("a&b|c=d&!a a True b False c True d True"));
         }
     }
 }
